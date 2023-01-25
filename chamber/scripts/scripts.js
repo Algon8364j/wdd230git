98f1b7224e244f4e08122717222f28110b549e58
Algon8364j <104524754+Algon8364j@users.noreply.github.com>
2023-01-25 14:28:49 +0000

    chamber

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,7 @@
+const mybtn = document.querySelector('header button')
+const mynav = document.querySelector('nav')
+
+mybtn.addEventListener('click',()=>{
+    mynav.classList.toggle('open')
+    mybtn.classList.toggle('open')
+}) 
